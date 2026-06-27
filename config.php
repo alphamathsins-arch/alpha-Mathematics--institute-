ba4f6a43943040cf24cb72d12089441b337a6fda
alphamathsins-arch <alphamathsins@gmail.com>
2026-06-27 22:38:27 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,19 @@
+<?php
 
+$host="localhost";
+
+$user="root";
+
+$password="";
+
+$database="alpha_maths";
+
+$conn=mysqli_connect($host,$user,$password,$database);
+
+if(!$conn){
+
+die("Database Connection Failed");
+
+}
+
+?>
