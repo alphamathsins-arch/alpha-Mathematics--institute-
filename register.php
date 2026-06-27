e8d3c14cb71588ce66d3bad031d673c244dc5c59
alphamathsins-arch <alphamathsins@gmail.com>
2026-06-27 22:40:09 +0200

    Implement user registration functionality
    This script processes user registration by inserting data into the 'students' table.

--- register.php
+++ register.php
@@ -1,1 +1,35 @@
+<?php
 
+include("config.php");
+
+$fullname=$_POST['fullname'];
+
+$email=$_POST['email'];
+
+$username=$_POST['username'];
+
+$password=password_hash($_POST['password'],PASSWORD_DEFAULT);
+
+$grade=$_POST['grade'];
+
+$session=$_POST['session'];
+
+$maths=$_POST['maths'];
+
+$science=$_POST['science'];
+
+$sql="INSERT INTO students(fullname,email,username,password,grade,session_type,maths,science)
+
+VALUES('$fullname','$email','$username','$password','$grade','$session','$maths','$science')";
+
+if(mysqli_query($conn,$sql)){
+
+header("Location: student-login.html");
+
+}else{
+
+echo "Registration Failed.";
+
+}
+
+?>
